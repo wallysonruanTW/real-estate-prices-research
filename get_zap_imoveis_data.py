5a53cbbfdded975c3006279e32d9d45be6db3ed2
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-26 21:19:04 -0300

    REFACTOR: Reusing function instead of declaring it again

--- get_zap_imoveis_data.py
+++ get_zap_imoveis_data.py
@@ -1,29 +1,7 @@
-import csv
+import utils
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-def getCsvNumberOfRows(csv_path):
-    row_count = -1
-
-    for row in open(csv_path):
-        row_count += 1
-
-    return row_count
-
-def saveToCsv(adress, price):
-    csv_path = "csv_files/zap_imoveis_data.csv"
-    headers = ["address", "price"]
-
-    with open(csv_path, "a") as zapimoveis_csv:
-        writer = csv.DictWriter(zapimoveis_csv, headers)
-
-        if getCsvNumberOfRows(csv_path) > 0:
-            writer.writerow({headers[0]: adress, headers[1]: price})
-            return
-
-        writer.writeheader()
-        writer.writerow({headers[0]: adress, headers[1]: price})
 
 # Link to the first page of the website, with the filter set to look for houses in São Paulo, SP, Brazil
 zap_imoveis_url = "https://www.zapimoveis.com.br/venda/imoveis/sp+sao-paulo/"
@@ -37,4 +15,4 @@ elements = parent_element.find_elements(By.CLASS_NAME, "card-container")
 for element in elements:
     price: str = element.find_elements(By.CLASS_NAME, "simple-card__price")[0].text
     address: str = element.find_elements(By.CLASS_NAME, "simple-card__address")[0].text
-    saveToCsv(address, price)
+    utils.save_to_csv(address, price, "csv_files/zap_imoveis_data.csv", ["address", "price"])
