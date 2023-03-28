279d8dfb5c93eaecc0a5d4185bae738c11dcd238
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-28 01:06:15 -0300

    FEAT: Implemented data cleaning to the zap imoveis data

--- src/scrappers/zap_imoveis/get_zap_imoveis_data.py
+++ src/scrappers/zap_imoveis/get_zap_imoveis_data.py
@@ -16,7 +16,7 @@ def get_csv_number_of_rows(csv_path):
     return row_count
 
 
-def save_to_csv(adress, price, date, csv_path):
+def save_raw_data_to_csv(adress, price, date, csv_path):
     headers = ["address", "price", "date"]
 
     open_mode = "a"
@@ -24,7 +24,6 @@ def save_to_csv(adress, price, date, csv_path):
     if os.path.isfile(csv_path) is False:
         open_mode = "x"
 
-    print(open_mode)
     with open(csv_path, open_mode) as zap_imoveis_csv:
         writer = csv.DictWriter(zap_imoveis_csv, headers)
 
@@ -36,8 +35,27 @@ def save_to_csv(adress, price, date, csv_path):
         writer.writerow({headers[0]: adress, headers[1]: price, headers[2]: date})
 
 
+def save_clean_data_to_csv(street, district, price, date, csv_path):
+    headers = ["street", "district", "price", "date"]
+
+    open_mode = "a"
+
+    if os.path.isfile(csv_path) is False:
+        open_mode = "x"
+
+    with open(csv_path, open_mode) as zap_imoveis_csv:
+        writer = csv.DictWriter(zap_imoveis_csv, headers)
+
+        if get_csv_number_of_rows(csv_path) > 0:
+            writer.writerow({headers[0]: street, headers[1]: district, headers[2]: price, headers[3]: date})
+            return
+
+        writer.writeheader()
+        writer.writerow({headers[0]: street, headers[1]: district, headers[2]: price, headers[3]: date})
+
+
 def get_street_and_district_from_address_column(address: str):
-    full_address = address.split()
+    full_address = address.split(",")
     separated_address = {
         "street": full_address[0],
         "district": full_address[1]
@@ -45,11 +63,11 @@ def get_street_and_district_from_address_column(address: str):
     return separated_address
 
 
-def get_only_the_numbers_from_price_column(price):
+def get_only_the_numbers_from_price_column(price:str):
     return price.replace("R$", "")
 
 
-def clean_zap_imoveis_data(raw_data_csv_path, path_to_store_the_cleaned_data):
+def clean_zap_imoveis_data(raw_data_csv_path:str, path_to_store_the_cleaned_data:str):
     csv_file = pandas.read_csv(raw_data_csv_path)
     addresses = csv_file["address"].tolist()
     prices = csv_file["price"].tolist()
@@ -58,14 +76,21 @@ def clean_zap_imoveis_data(raw_data_csv_path, path_to_store_the_cleaned_data):
     i = 0
 
     while i < len(addresses):
-        save_to_csv(addresses[i], prices[i], dates[i], path_to_store_the_cleaned_data)
+        price = get_only_the_numbers_from_price_column(prices[i])
+        address = get_street_and_district_from_address_column(addresses[i])
+        street = address["street"].strip()
+        district = address["district"].strip()
+
+        save_clean_data_to_csv(street, district, price, dates[i], path_to_store_the_cleaned_data)
         i += 1
 
 
 # Link to the first page of the website, with the filter set to look for houses in São Paulo, SP, Brazil
 zap_imoveis_url = "https://www.zapimoveis.com.br/venda/imoveis/sp+sao-paulo/"
-raw_data_csv_path = "../../../data/raw/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(date=datetime.date.today())
-cleaned_data_csv_path = "../../../data/cleaned/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(date=datetime.date.today())
+raw_data_csv_path = "../../../data/raw/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(
+    date=datetime.date.today())
+cleaned_data_csv_path = "../../../data/cleaned/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(
+    date=datetime.date.today())
 
 browser = webdriver.Chrome()
 browser.get(zap_imoveis_url)
@@ -76,7 +101,6 @@ elements = parent_element.find_elements(By.CLASS_NAME, "card-container")
 for element in elements:
     price: str = element.find_elements(By.CLASS_NAME, "simple-card__price")[0].text
     address: str = element.find_elements(By.CLASS_NAME, "simple-card__address")[0].text
-    save_to_csv(address, price, datetime.date.today(), raw_data_csv_path)
-    print("oi")
+    save_raw_data_to_csv(address, price, datetime.date.today(), raw_data_csv_path)
 
 clean_zap_imoveis_data(raw_data_csv_path, cleaned_data_csv_path)
