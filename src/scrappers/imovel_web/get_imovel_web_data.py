517c26d2ef3b86bb0088e0663cf9d85a2be431e1
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-27 22:22:17 -0300

    REFACTOR: Updated the CSV file paths

--- src/scrappers/imovel_web/get_imovel_web_data.py
+++ src/scrappers/imovel_web/get_imovel_web_data.py
@@ -1,20 +1,20 @@
-import utils
+import utils.csv_utils
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # Link to the first page of the website, with the filter set to look for houses in São Paulo, SP, Brazil
 imoveis_web_url = "https://www.imovelweb.com.br/imoveis-venda-sao-paulo-sp.html?iv_=__iv_p_1_a_14183648872_g_131270747251_w_kwd-320060452994_h_1001566_ii__d_c_v__n_g_c_537751034622_k_imovelweb_m_e_l__t__e__r__vi__&gclid=Cj0KCQjw2v-gBhC1ARIsAOQdKY1y8mXhbQGbaj3mFfLv2-Wh7lRkQ-v63JxJQnSpZUJhxE0Dh547e0gaAmxpEALw_wcB"
+csv_path = "../../../data/raw/real_state/imovel_web/imoveis_web_data.csv"
+csv_headers = ["address", "price"]
 
 browser = webdriver.Chrome()
 browser.get(imoveis_web_url)
 
 parent_element = browser.find_elements(By.XPATH, r'/html/body/div[1]/div[2]/div/div/div[2]/div[2]/div[2]')[0]
-
 elements = parent_element.find_elements(By.CLASS_NAME, r'sc-i1odl-2')
 
 for element in elements:
-
     price = element.find_elements(By.CLASS_NAME, r'sc-12dh9kl-4')
     address = element.find_elements(By.CLASS_NAME, r'sc-ge2uzh-1')
 
@@ -25,4 +25,4 @@ for element in elements:
         address = address[0].text
 
     if len(price) > 0 and len(address) > 0:
-        utils.save_to_csv(address, price, "csv_files/imoveis_web_data.csv", ["address", "price"])
+        utils.csv_utils.save_to_csv(address, price, csv_path, csv_headers)
--- src/scrappers/zap_imoveis/get_zap_imoveis_data.py
+++ src/scrappers/zap_imoveis/get_zap_imoveis_data.py
@@ -1,10 +1,12 @@
-import utils
+import utils.csv_utils
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # Link to the first page of the website, with the filter set to look for houses in São Paulo, SP, Brazil
 zap_imoveis_url = "https://www.zapimoveis.com.br/venda/imoveis/sp+sao-paulo/"
+csv_path = "../../../data/raw/real_state/zap_imoveis/zap_imoveis_data.csv"
+csv_headers = ["address", "price"]
 
 browser = webdriver.Chrome()
 browser.get(zap_imoveis_url)
@@ -15,4 +17,4 @@ elements = parent_element.find_elements(By.CLASS_NAME, "card-container")
 for element in elements:
     price: str = element.find_elements(By.CLASS_NAME, "simple-card__price")[0].text
     address: str = element.find_elements(By.CLASS_NAME, "simple-card__address")[0].text
-    utils.save_to_csv(address, price, "csv_files/zap_imoveis_data.csv", ["address", "price"])
+    utils.csv_utils.save_to_csv(address, price, csv_path, csv_headers)
