58caeccc83b2e3a5c052cd136f3bb37c2b3bebe0
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-28 14:14:54 -0300

    REFACTOR: Extracted the IMOVEL WEB data cleaner to a file of its own

--- src/scrappers/imovel_web/get_imovel_web_data.py
+++ src/scrappers/imovel_web/get_imovel_web_data.py
@@ -1,13 +1,23 @@
 import csv
 import datetime
 import os.path
-import pandas
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from utils import csv_utils
 
-def save_raw_data_to_csv(adress, price, date, csv_path):
+
+def save_raw_data_to_csv(adress: str, price: str, date: datetime.date, csv_path: str) -> None:
+    """Create a csv file if none exists, and name it accordingly to the following format:
+    [date_of_execution]_zap_imoveis_data.csv
+
+    :param adress:
+    :param price:
+    :param date:
+    :param csv_path:
+    :return: None
+    """
+
     headers = ["address", "price", "date"]
 
     open_mode = "a"
@@ -26,62 +36,12 @@ def save_raw_data_to_csv(adress, price, date, csv_path):
         writer.writerow({headers[0]: adress, headers[1]: price, headers[2]: date})
 
 
-def save_clean_data_to_csv(street, district, price, date, csv_path):
-    headers = ["street", "district", "price", "date"]
-
-    open_mode = "a"
-
-    if os.path.isfile(csv_path) is False:
-        open_mode = "x"
-
-    with open(csv_path, open_mode) as imovel_web_csv:
-        writer = csv.DictWriter(imovel_web_csv, headers)
-
-        if csv_utils.get_csv_number_of_rows(csv_path) > 0:
-            writer.writerow({headers[0]: street, headers[1]: district, headers[2]: price, headers[3]: date})
-            return
-
-        writer.writeheader()
-        writer.writerow({headers[0]: street, headers[1]: district, headers[2]: price, headers[3]: date})
-
-
-def get_street_and_district_from_address_column(address: str):
-    full_address = address.split(",")
-    separated_address = {
-        "street": full_address[0],
-        "district": full_address[-1]
-    }
-    return separated_address
-
-
-def get_only_the_numbers_from_price_column(price:str):
-    return price.replace("R$", "")
-
-
-def clean_imovel_web_data(raw_data_csv_path:str, path_to_store_the_cleaned_data:str):
-    csv_file = pandas.read_csv(raw_data_csv_path)
-    addresses = csv_file["address"].tolist()
-    prices = csv_file["price"].tolist()
-    dates = csv_file["date"].tolist()
-
-    i = 0
-
-    while i < len(addresses):
-        price = get_only_the_numbers_from_price_column(prices[i])
-        address = get_street_and_district_from_address_column(addresses[i])
-        street = address["street"].strip()
-        district = address["district"].strip()
-
-        save_clean_data_to_csv(street, district, price, dates[i], path_to_store_the_cleaned_data)
-        i += 1
-
 # Link to the first page of the website, with the filter set to look for houses in São Paulo, SP, Brazil
 imoveis_web_url = "https://www.imovelweb.com.br/imoveis-venda-sao-paulo-sp.html?iv_" \
                   "=__iv_p_1_a_14183648872_g_131270747251_w_kwd" \
                   "-320060452994_h_1001566_ii__d_c_v__n_g_c_537751034622_k_imovelweb_m_e_l__t__e__r__vi__&gclid" \
                   "=Cj0KCQjw2v-gBhC1ARIsAOQdKY1y8mXhbQGbaj3mFfLv2-Wh7lRkQ-v63JxJQnSpZUJhxE0Dh547e0gaAmxpEALw_wcB "
 raw_data_csv_path = "../../../data/raw/real_state/imovel_web/{date}_imoveis_web_data.csv".format(date=datetime.date.today())
-clean_data_csv_path = "../../../data/clean/real_state/imovel_web/{date}_imoveis_web_data.csv".format(date=datetime.date.today())
 
 browser = webdriver.Chrome()
 browser.get(imoveis_web_url)
@@ -107,5 +67,3 @@ for element in elements:
         address = street_and_door_number + ", " + district_and_city
 
     save_raw_data_to_csv(address, price, datetime.date.today(), raw_data_csv_path)
-
-clean_imovel_web_data(raw_data_csv_path, clean_data_csv_path)
