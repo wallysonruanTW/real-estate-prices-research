de438623ed730c4287edf8d525f32febf6cd0113
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-30 21:55:35 -0300

    REFACTOR: Improved the method of saving the CSV and created  a exclusive class

--- src/data_cleaners/zap_imoveis/data_cleaner_zap_imoveis.py
+++ src/data_cleaners/zap_imoveis/data_cleaner_zap_imoveis.py
@@ -1,64 +1,45 @@
-import csv
 import datetime
-import os
+
 import pandas
 
-from utils import csv_utils
+from src.entities.RealStateClean import RealStateClean
 
 
-def save_clean_data_to_csv(street, district, price, date, csv_path):
-    headers = ["street", "district", "price", "date"]
-
-    open_mode = "a"
-
-    if os.path.isfile(csv_path) is False:
-        open_mode = "x"
-
-    with open(csv_path, open_mode) as zap_imoveis_csv:
-        writer = csv.DictWriter(zap_imoveis_csv, headers)
-
-        if csv_utils.get_csv_number_of_rows(csv_path) > 0:
-            writer.writerow({headers[0]: street, headers[1]: district, headers[2]: price, headers[3]: date})
-            return
-
-        writer.writeheader()
-        writer.writerow({headers[0]: street, headers[1]: district, headers[2]: price, headers[3]: date})
-
-
-def get_street_and_district_from_address_column(address: str):
+def get_street_without_door_number(address: str):
     full_address = address.split(",")
-    separated_address = {
-        "street": full_address[0],
-        "district": full_address[1]
-    }
-    return separated_address
+    street = "".join((letter for letter in full_address[0] if not letter.isdigit()))
+    return street
 
 
-def get_only_the_numbers_from_price_column(price:str):
-    return price.replace("R$", "")
+def get_district(address: str):
+    full_address = address.split(",")
+    return full_address[-1]
 
 
-def clean_zap_imoveis_data(raw_data_csv_path:str, path_to_store_the_cleaned_data:str):
-    csv_file = pandas.read_csv(raw_data_csv_path)
-    addresses = csv_file["address"].tolist()
-    prices = csv_file["price"].tolist()
-    dates = csv_file["date"].tolist()
-
-    i = 0
-
-    while i < len(addresses):
-        price = get_only_the_numbers_from_price_column(prices[i])
-        address = get_street_and_district_from_address_column(addresses[i])
-        street = address["street"].strip()
-        district = address["district"].strip()
-
-        save_clean_data_to_csv(street, district, price, dates[i], path_to_store_the_cleaned_data)
-        i += 1
+def get_only_the_numbers_from_price(price: str):
+    return price.replace("R$", "").replace(" ", "")
 
 
 raw_data_csv_path: str = "../../../data/raw/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(
     date=datetime.date.today())
-cleaned_data_csv_path: str = "../../../data/clean/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(
+clean_data_csv_path: str = "../../../data/clean/real_state/zap_imoveis/{date}_zap_imoveis_data.csv".format(
     date=datetime.date.today())
 
-clean_zap_imoveis_data(raw_data_csv_path, cleaned_data_csv_path)
+
+csv_file = pandas.read_csv(raw_data_csv_path)
+addresses = csv_file["address"].tolist()
+prices = csv_file["price"].tolist()
+dates = csv_file["dateOfCreation"].tolist()
+
+real_states_clean = []
+i = 0
+while i < len(addresses):
+    price = get_only_the_numbers_from_price(prices[i])
+    street = get_street_without_door_number(addresses[i])
+    district = get_district(addresses[1])
+
+    real_states_clean.append(RealStateClean(street, district, price, dates[i]))
+    i += 1
+
+real_states_data_frame = pandas.DataFrame(real_state.__dict__ for real_state in real_states_clean)
+real_states_data_frame.to_csv(clean_data_csv_path)
